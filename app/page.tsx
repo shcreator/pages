4126829349e21fd0fca1ce766f62845150158bc0
shcreator <mainid24@outlook.com>
2024-10-09 17:27:31 +0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
-import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <h2 id="more">Learn More</h2>
-      <p>
-        You can optionally configure a cloud provider, device sizes, and more!
-      </p>
+    <div>
+      Hello World. <Link href="/about">About</Link>
+    </div>
   );
 }
-
-export default Home;

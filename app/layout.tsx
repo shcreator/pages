26d347cc14cf7bfee3a2c016579ba0e39824efac
shcreator <mainid24@outlook.com>
2024-10-09 17:00:22 +0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { GoogleTagManager } from "@next/third-parties/google";
+import { analytics } from '../lib/firebase';
 
 const inter = Inter({ subsets: ["latin"] });
 

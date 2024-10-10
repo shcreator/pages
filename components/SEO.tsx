6d379ead727bf5691bf198c7e1719e987933b05b
shcreator <mainid24@outlook.com>
2024-10-10 15:23:14 +0600

    Create SEO.tsx

---
+++ components/SEO.tsx
@@ -0,0 +1,35 @@
+import Head from 'next/head';
+
+interface SEOProps {
+  title: string;
+  description: string;
+  url: string;
+  image: string;
+}
+
+const SEO = ({ title, description, url, image }: SEOProps) => {
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" />
+
+      {/* Open Graph / Facebook */}
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content={url} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={image} />
+
+      {/* Twitter */}
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content={url} />
+      <meta property="twitter:title" content={title} />
+      <meta property="twitter:description" content={description} />
+      <meta property="twitter:image" content={image} />
+    </Head>
+  );
+};
+
+export default SEO;
